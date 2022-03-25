86907a2c6e233b14105bcd141da2aaf0a3d02928
AidarKhafizov <91023295+AidarKhafizov@users.noreply.github.com>
2022-03-26 00:39:45 +0300

    1

--- Main.java
+++ Main.java
@@ -8,10 +8,10 @@ public class Main {
 
     public static void main(String args[]) throws IOException {
 
-        File f = new File("t.txt");
+        File f = new File("t1.txt");
         File f1 = new File("2.txt");
         FileWriter writer = new FileWriter(f1);
-        int[] temparr = {49, 369, 1095, 1135, 1160, 1169, 1638, 1649, 1807, 2151, 2261, 2436, 2491, 2622, 2886, 2999, 3226, 3870, 3906, 4068, 4325, 4356, 4361, 4439, 4574, 4759, 4766, 4809, 4914, 5174, 5252, 5727, 5890, 6225, 6302, 6448, 6697, 6750, 6806, 8386, 8650, 9000, 9264, 9529, 9579, 9581, 9654, 9668, 9684, 9762};
+        int[] temparr = {31, 370, 1076, 1159, 1160, 1169, 1638, 1649, 1809, 2051, 2259, 2442, 2491, 2622, 2886, 2999, 3226, 3870, 3906, 4068, 4325, 4356, 4361, 4439, 4574, 4759, 4766, 4809, 4914, 5174, 5252, 5727, 5890, 6225, 6302, 6448, 6697, 6750, 6806, 8386, 8650, 9000, 9264, 9529, 9579, 9581, 9654, 9668, 9684, 9762};
         for (int i = 0; i < 50; i++) {
             Rand.rand(f, temparr[i]);
             writer.append(String.valueOf(temparr[i])).append(",");
@@ -24,6 +24,6 @@ public class Main {
             writer.append(String.valueOf(System.nanoTime() - a1)).append("\n");
             //System.out.println(Arrays.toString(a));
         }
-        writer.flush();
+        writer.close();
     }
 }
